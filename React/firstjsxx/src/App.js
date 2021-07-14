313fb32a858fbe966b4fcdfaf3accdbc9b81a09d
Abdulrahman-ghouli <a.e.ghouli@gmail.com>
2021-07-14 15:23:33 +0300

    updated

---
+++ React/firstjsxx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo !</h1>
+      <h2> Things i need to do :</h2> 
+      <ul>
+      <li>Learn React</li>
+      <li>Clim everst</li>
+      <li>run a marathon</li>
+      <li>feed a dog</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
